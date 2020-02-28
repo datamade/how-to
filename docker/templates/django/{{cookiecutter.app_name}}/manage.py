daf88639ce231d5966ffda8e7f8dd1f5737c08b2
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-02-28 17:43:03 -0600

    Switch to using docker-entrypoint for migrations

--- docker/templates/django/{{cookiecutter.app_name}}/manage.py
+++ docker/templates/django/{{cookiecutter.app_name}}/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.module_name}}.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
