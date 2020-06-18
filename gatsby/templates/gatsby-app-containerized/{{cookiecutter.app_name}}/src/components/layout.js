e99d1a24640b8a78aa26118ea722f632ef535e3b
Bea Malsky <beamalsky@datamade.us>
2020-06-18 17:04:39 -0500

    Add test action to run linter on each commit

--- gatsby/templates/gatsby-app-containerized/{{cookiecutter.app_name}}/src/components/layout.js
+++ gatsby/templates/gatsby-app-containerized/{{cookiecutter.app_name}}/src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Header from "./header"
 import "./layout.css"
