f753dbfb592d4ab03c16c26fb1ae484c0f0ac49b
msj <monkruman.st.jules@datamde.us>
2022-09-22 15:22:55 -0400

    Add robots.txt logic

--- docker/templates/new-wagtail-app/{{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/views.py
+++ docker/templates/new-wagtail-app/{{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/views.py
@@ -1,20 +1,33 @@
+import os
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class ReactView(TemplateView):
-    title = 'React Page'
-    template_name = '{{ cookiecutter.module_name }}/react_page.html'
-    component = 'js/App.js'
+    title = "React Page"
+    template_name = "{{ cookiecutter.module_name }}/react_page.html"
+    component = "js/App.js"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         return context
 
 
-def page_not_found(request, exception, template_name='{{ cookiecutter.module_name }}/404.html'):
+def robots_txt(request):
+    return render(
+        request,
+        "{{ cookiecutter.module_name }}/robots.txt",
+        {"ALLOW_CRAWL": True if os.getenv("ALLOW_CRAWL") == "True" else False},
+        content_type="text/plain",
+    )
+
+
+def page_not_found(
+    request, exception, template_name="{{ cookiecutter.module_name }}/404.html"
+):
     return render(request, template_name, status=404)
 
 
-def server_error(request, template_name='{{ cookiecutter.module_name }}/500.html'):
+def server_error(request, template_name="{{ cookiecutter.module_name }}/500.html"):
     return render(request, template_name, status=500)
