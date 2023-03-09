11d40cd5402f982e28c3af20def069251336b049
Sam McAlilly <smcalilly@gmail.com>
2023-03-09 14:36:56 -0600

    don't use latest and revert settings change

--- docker/templates/new-django-app/{{cookiecutter.app_name}}/{{cookiecutter.module_name}}/settings.py
+++ docker/templates/new-django-app/{{cookiecutter.app_name}}/{{cookiecutter.module_name}}/settings.py
@@ -164,16 +164,10 @@ STATICFILES_FINDERS = (
 # Add them to the "only" array in babel.config.json, as documented in the
 # README under "Ensuring browser compatibility".
 COMPRESS_PRECOMPILERS = (
-    (
-        "module",
-        "export NODE_PATH=/app/node_modules && npx browserify -g browserify-css {infile} -t \
-            [ babelify --presets [ @babel/preset-env ] ] > {outfile}",
-    ),
-    (
-        "text/jsx",
-        "export NODE_PATH=/app/node_modules && npx browserify -g browserify-css {infile} -t \
-            [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] > {outfile}",
-    ),
+    ('module',
+     'export NODE_PATH=/app/node_modules && npx browserify {infile} -t [ babelify --global --presets [ @babel/preset-env ] ] > {outfile}'),
+    ('text/jsx',
+     'export NODE_PATH=/app/node_modules && npx browserify {infile} -t [ babelify --global --presets [ @babel/preset-env @babel/preset-react ] ] > {outfile}'),
 )
 
 COMPRESS_OUTPUT_DIR = 'compressor'
