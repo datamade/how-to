b39e33226e7cb4dbddc4b4282740b02c9477be11
msj <monkruman.st.jules@datamde.us>
2023-07-13 11:27:03 -0400

    Add code formatting/linting tests to CI

--- docker/templates/new-django-app/{{cookiecutter.app_name}}/scripts/run-tests.sh
+++ docker/templates/new-django-app/{{cookiecutter.app_name}}/scripts/run-tests.sh
@@ -2,8 +2,6 @@
 result=0
 trap 'result=1' ERR
 
-flake8 {{cookiecutter.module_name}} tests
-npx eslint {{cookiecutter.module_name}}/static/js/*.js
 pytest -sxv
 
 exit "$result"
--- docker/templates/new-wagtail-app/{{ cookiecutter.app_name }}/scripts/run-tests.sh
+++ docker/templates/new-wagtail-app/{{ cookiecutter.app_name }}/scripts/run-tests.sh
@@ -2,7 +2,6 @@
 result=0
 trap 'result=1' ERR
 
-flake8 {{ cookiecutter.module_name }} tests
 pytest -sxv
 
 exit "$result"
